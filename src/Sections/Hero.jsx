1018e6789892e8439f4f8f110f6b138fdd2bc5d1
arnavos <email>
2024-05-09 20:33:05 +0530

    test

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -46,12 +46,8 @@ function Hero() {
                         })}
                     </ul>
                 </div>
-<<<<<<< HEAD
                 {(window.innerWidth > 1000) && <figure className="hero-banner"> <Humo /> </figure>}
                 {true && <figure className="hero-banner"> <Humo /> </figure>}
-=======
-                <figure className="hero-banner"> <Humo /> </figure>
->>>>>>> 787ae3c8fd84878ce2514b14dfcca776c36f9a21
             </div>
         </section>
     )
